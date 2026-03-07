9ac1165ea290e5151a882712a81f7a88fdd4bdf2
SeanLenardB <cordysean@163.com>
2026-03-07 18:28:55 +0800

    fix: follower count changed to double, show chart id

--- RelicsAdofai/RelicsAdofai/Game/Chart.cs
+++ RelicsAdofai/RelicsAdofai/Game/Chart.cs
@@ -6,12 +6,13 @@ namespace RelicsAdofai.Game
 {
     public class Chart
     {
+        public int Id = 0;
         public string Artist = "";
         public string Song = "";
         public string Creator = "";
 
         public double RequiredSkill = 0.0;
 
-        public override string ToString() { return $"{Artist} - {Song} [{Creator}]"; }
+        public override string ToString() { return $"#{this.Id}. {this.Artist} - {this.Song} [{this.Creator}]"; }
     }
 }
