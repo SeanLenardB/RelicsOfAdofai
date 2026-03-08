ea12f33d040ce3084a6264ecb0174ddfbf1a5ff4
SeanLenardB <cordysean@163.com>
2026-03-08 15:52:46 +0800

    fix: progressioned sc events not having correct color for money

--- RelicsAdofai/RelicsAdofai/Game/GameContext.Progression.cs
+++ RelicsAdofai/RelicsAdofai/Game/GameContext.Progression.cs
@@ -4,6 +4,7 @@
     {
         public bool Unlocked_ScIncome_Tier1 = false;
         public bool Unlocked_ScIncome_Tier2 = false;
+        public bool Unlocked_ScIncome_Tier3 = false;
         public void UpdateProgression()
         {
             if (!this.Unlocked_ScIncome_Tier1 && this.FollowerCount > 100)
@@ -14,11 +15,11 @@
                     double moneyAmount = this.Random.NextDouble() * 10;
                     return ChoiceEvent.Info(
                         $"充电通知",
-                        $"你的一位粉丝给你充电{moneyAmount:0.00}元",
+                        $"<p>你的一位粉丝给你充电<span class=\"color-money\">{moneyAmount:0.00}</span></p>",
                         context => context.Money += moneyAmount);
                 }));
             }
-            if (!this.Unlocked_ScIncome_Tier2 && this.FollowerCount > 1000)
+            if (!this.Unlocked_ScIncome_Tier2 && this.FollowerCount > 5000)
             {
                 this.Unlocked_ScIncome_Tier2 = true;
                 this.ChoiceEventWeights.Add(new(1, () =>
@@ -26,7 +27,7 @@
                     double moneyAmount = this.Random.NextDouble() * 10;
                     return ChoiceEvent.Info(
                         $"充电通知",
-                        $"你的一位粉丝给你充电{moneyAmount:0.00}元",
+                        $"<p>你的一位粉丝给你充电<span class=\"color-money\">{moneyAmount:0.00}</span></p>",
                         context => context.Money += moneyAmount);
                 }));
                 this.ChoiceEventWeights.Add(new(1, () =>
@@ -34,7 +35,7 @@
                     double moneyAmount = this.Random.NextDouble() * 50;
                     return ChoiceEvent.Info(
                         $"充电通知",
-                        $"你的一位粉丝给你充电{moneyAmount:0.00}元",
+                        $"<p>你的一位粉丝给你充电<span class=\"color-money\">{moneyAmount:0.00}</span></p>",
                         context =>
                         {
                             context.Money += moneyAmount;
